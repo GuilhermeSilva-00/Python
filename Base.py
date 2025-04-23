b2676f6cbd303d6319854588c4cd8b46b68112e5
GuilhermeSilva-00 <contato.gvas@gmail.com>
2025-04-22 21:22:20 -0300

    Atualização

--- Base.py
+++ Base.py
@@ -1,21 +1,4 @@
-import pandas as pd
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-filename = "teste-457522-8ebf140dff6b.json"
-scopes = [ 
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    filename=filename,
-    scopes=scopes
-)
-
-client = gspread.authorize(creds)
-print(client)
 
 
-planilha= client.open(title="Teste", 
-                      folder_id="1KHbn5BGdsov73OO32NIbWFVViwt8_PR9")
+"""planilha= client.open(title="Teste", 
+                      folder_id="1KHbn5BGdsov73OO32NIbWFVViwt8_PR9") """
